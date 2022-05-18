1ada6709f630eef06ef9197a1422da4ca12dca7a
AinhoaBaron <93262452+AinhoaBaron@users.noreply.github.com>
2022-05-18 14:29:36 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,16 +3,24 @@ DFRobotMaqueenPlus.I2CInit()
 basic.forever(function () {
     if (distantzia > 60) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 200)
-    } else if (distantzia > 40) {
+    } else if (distantzia > 50) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
-    } else if (distantzia > 20) {
+    } else if (distantzia > 40) {
         DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 50)
+    } else if (distantzia > 30) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 40)
+    } else if (distantzia > 20) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+    } else if (distantzia > 15) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 20)
     } else if (distantzia > 10) {
-        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 10)
     } else if (distantzia > 5) {
-        DFRobotMaqueenPlus.mototStop(Motors.ALL)
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 0)
+    } else if (distantzia > 2) {
+        DFRobotMaqueenPlus.mototStop(Motors.M1)
     }
 })
 basic.forever(function () {
-    distantzia = DFRobotMaqueenPlus.ultraSonic(PIN.P0, PIN.P0)
+    distantzia = DFRobotMaqueenPlus.ultraSonic(PIN.P1, PIN.P2)
 })
